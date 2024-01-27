ed6ba64c068489428ed698b6f2960fda059688e4
Sidharth <imadethisjustfororiginbruh@gmail.com>
2024-01-27 15:48:58 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,27 @@ const TAMU = require("../database/TAMU.json");
 app.use(express.static("../public/pages/home"));
 
 app.get('/', (req, res) => {
+    
     res.sendFile(path.join(__dirname, "../public/pages/home/index.html"));
 });
 
-app.listen(port, (console.log(`Listening on port ${port}`)));
+app.get('/college/:name/:major/:year', async (req, res) => {
+    const { name, major, year } = req.params;
+
+    let files = fs.readdirSync("../database/");
+    
+    for (let i = 0; i < files.length; i++) {
+        if (`${name}.json` == files[i]) {
+            
+        }
+    }
+
+    let resObj = {
+
+    }
+});
+
+
+app.listen(port, () => {
+    
+});

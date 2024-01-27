9bed4977f0c5c8bf6ccd233de49f35883bed8468
Sidharth <imadethisjustfororiginbruh@gmail.com>
2024-01-27 14:42:39 -0600

    Update index.js

---
+++ server/index.js
@@ -0,0 +1,15 @@
+{
+    CollegeName,
+    Majors[
+        Major {
+            Courses[
+                coursename,
+                prereq[],
+                credits
+            ],
+            CreditsReq,
+
+        }
+    ],
+
+}

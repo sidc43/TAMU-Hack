992ddf11b367ef6e0dd4dfe7cd95f36e44c2cbbe
Sidharth <imadethisjustfororiginbruh@gmail.com>
2024-01-27 15:34:14 -0600

    Setup server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 const app = express();
 const port = require("./server.json").port;
+const TAMU = require("../database/TAMU.json");
+
+app.use(express.static("../public/pages/home"));
 
 app.get('/', (req, res) => {
-    res.send("Test");
+    res.sendFile(path.join(__dirname, "../public/pages/home/index.html"));
 });
 
 app.listen(port, (console.log(`Listening on port ${port}`)));
